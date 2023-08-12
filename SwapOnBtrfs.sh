804253f7f7a7f7e4687bbc031a3a49b1eb25ff50
Julio Vanderlei <chartsw@hotmail.com>
2023-08-12 17:30:56 -0300

    Update SwapOnBtrfs.sh

--- SwapOnBtrfs.sh
+++ SwapOnBtrfs.sh
@@ -6,11 +6,11 @@ mkdir /swap
 # remove COW in /swap
 chattr +C /swap
 # create swapfile 1G in /swap
-dd if=/dev/zero of=/swap/swapfile bs=1M count=1024 status=progress
-# permissions
-chmod 0600 /swap/swapfile
+sudo fallocate -l 1G /swap/swapfile
+# set permissions
+chmod 600 /swap/swapfile
 # turn swap on
-mkswap -U clear /swap/swapfile
+mkswap /swap/swapfile
 swapon /swap/swapfile
 # add entry in fstab config
-echo /swap/swapfile none swap defaults 0 0 >> /etc/fstab
+echo /swap/swapfile swap swap defaults 0 0 >> /etc/fstab
