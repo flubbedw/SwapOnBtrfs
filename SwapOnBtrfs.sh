d308974ef125b23ba1d9182be482c8211509b37e
Julio Vanderlei <chartsw@hotmail.com>
2023-08-13 13:31:51 -0300

    Update SwapOnBtrfs.sh

--- SwapOnBtrfs.sh
+++ SwapOnBtrfs.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 # EXEC WITH SUDO PERMISSIONS
-
+if (( $EUID != 0 ));
+then
+    echo "Please run as root"
+    exit
+fi
 # create /swap directory
-mkdir /swap
+if [ -d "/swap" ]
+then
+    echo "Directory already exists, skipping."
+else
+    mkdir /swap
+fi
 # remove COW in /swap
 chattr +C /swap
 # create swapfile 1G in /swap
 sudo fallocate -l 1G /swap/swapfile
-# set permissions
+# permissions
 chmod 600 /swap/swapfile
 # turn swap on
 mkswap /swap/swapfile
 swapon /swap/swapfile
 # add entry in fstab config
-echo /swap/swapfile swap swap defaults 0 0 >> /etc/fstab
+STRING="/swap/swapfile none swap defaults 0 0"
+FILE="/etc/fstab"
+if grep -q "$STRING" "$FILE" ;
+then
+    echo "Entry already exists, skipping."
+else
+    echo /swap/swapfile none swap defaults 0 0 >> /etc/fstab
+fi
+exit 0
